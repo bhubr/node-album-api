49f4350b26d85a89b8bc93f0d231b4fdc1b9d56d
Benoît Hubert <benoithubert@gmail.com>
2021-12-16 22:03:46 +0100

    Refactor tests

--- test/auth.routes.test.ts
+++ test/auth.routes.test.ts
@@ -6,79 +6,65 @@ import jwtDecode from 'jwt-decode';
 
 import app from '../src/app';
 import { User } from '../src/entity/User';
+import { postRequest } from './request-helpers';
 
 const createUser = async (email, clearPassword) => {
   const repository = getRepository(User);
   const password = await hash(clearPassword, 8);
   const user = repository.create({
     email,
-    password
+    password,
   });
   await repository.save(user);
 };
 
 describe('auth routes', () => {
-
   // Register route
   describe('register', () => {
-    it('fails with missing parameters', () => request(app)
-      .post('/api/v2/auth/register')
-      .send({})
-      .set('Accept', 'application/json')
-      .expect(400)
-      .expect('Content-Type', /json/)
-      .then((res: any) => {
+    it('fails with missing parameters', () =>
+      postRequest('/v2/auth/register', {}, 400).then((res: any) => {
         expect(res.body.errors).to.be.a('array');
         expect(res.body.errors.length).to.equal(2);
-      })
-    );
+      }));
 
-    it('fails with an invalid email', () => request(app)
-      .post('/api/v2/auth/register')
-      .send({ login: 'john', pwd: '12345' })
-      .set('Accept', 'application/json')
-      .expect(400)
-      .expect('Content-Type', /json/)
-      .then((res: any) => {
+    it('fails with an invalid email', () => () =>
+      postRequest(
+        '/v2/auth/register',
+        { login: 'john', pwd: '12345' },
+        400
+      ).then((res: any) => {
         expect(res.body.errors).to.be.a('array');
         expect(res.body.errors.length).to.equal(1);
-      })
-    );
+      }));
 
-    it('fails with a short password', () => request(app)
-      .post('/api/v2/auth/register')
-      .send({ login: 'john@example.com', pwd: '123' })
-      .set('Accept', 'application/json')
-      .expect(400)
-      .expect('Content-Type', /json/)
-      .then((res: any) => {
+    it('fails with a short password', () =>
+      postRequest(
+        '/v2/auth/register',
+        { login: 'john@example.com', pwd: '123' },
+        400
+      ).then((res: any) => {
         expect(res.body.errors).to.be.a('array');
         expect(res.body.errors.length).to.equal(1);
-      })
-    );
+      }));
 
     it('fails with an existing email', async () => {
       await createUser('duplicate@example.com', 'abc12');
-      return request(app)
-        .post('/api/v2/auth/register')
-        .send({ login: 'duplicate@example.com', pwd: 'abc12' })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(409)
-      }
-    );
+      return postRequest(
+        '/v2/auth/register',
+        { login: 'duplicate@example.com', pwd: 'abc12' },
+        409
+      );
+    });
 
-    it('succeeds with correct parameters', () => request(app)
-      .post('/api/v2/auth/register')
-      .send({ login: 'will@example.com', pwd: 'abc12' })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(201)
-      .then((res: any) => {
+    it('succeeds with correct parameters', () =>
+      postRequest(
+        '/v2/auth/register',
+        { login: 'will@example.com', pwd: 'abc12' },
+        201
+      ).then((res: any) => {
         expect(res.body).to.be.a('object');
         expect(Object.keys(res.body).length).to.equal(0);
-      })
-    );
+      }));
   });
 
   // Login route
@@ -87,70 +73,54 @@ describe('auth routes', () => {
       await createUser('rose@example.com', 'abc12');
     });
 
-    it('fails with missing parameters', () => request(app)
-      .post('/api/v2/auth/login')
-      .send({})
-      .set('Accept', 'application/json')
-      .expect(400)
-      .expect('Content-Type', /json/)
-      .then((res: any) => {
+    it('fails with missing parameters', () =>
+      postRequest('/v2/auth/login', {}, 400).then((res: any) => {
         expect(res.body.errors).to.be.a('array');
         expect(res.body.errors.length).to.equal(2);
-      })
-    );
+      }));
 
-    it('fails with an invalid email', () => request(app)
-      .post('/api/v2/auth/login')
-      .send({ login: 'john', pwd: '12345' })
-      .set('Accept', 'application/json')
-      .expect(400)
-      .expect('Content-Type', /json/)
-      .then((res: any) => {
+    it('fails with an invalid email', () =>
+      postRequest('/v2/auth/login', { login: 'john', pwd: '12345' }, 400).then(
+        (res: any) => {
+          expect(res.body.errors).to.be.a('array');
+          expect(res.body.errors.length).to.equal(1);
+        }
+      ));
+
+    it('fails with a short password', () =>
+      postRequest(
+        '/v2/auth/login',
+        { login: 'john@example.com', pwd: '123' },
+        400
+      ).then((res: any) => {
         expect(res.body.errors).to.be.a('array');
         expect(res.body.errors.length).to.equal(1);
-      })
-    );
+      }));
 
-    it('fails with a short password', () => request(app)
-      .post('/api/v2/auth/login')
-      .send({ login: 'john@example.com', pwd: '123' })
-      .set('Accept', 'application/json')
-      .expect(400)
-      .expect('Content-Type', /json/)
-      .then((res: any) => {
-        expect(res.body.errors).to.be.a('array');
-        expect(res.body.errors.length).to.equal(1);
-      })
-    );
+    it('fails with non-existing email', () =>
+      postRequest(
+        '/v2/auth/login',
+        { login: 'john@example.com', pwd: 'abc12' },
+        401
+      ));
 
-    it('fails with non-existing email', () => request(app)
-      .post('/api/v2/auth/login')
-      .send({ login: 'john@example.com', pwd: 'abc12' })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(401)
-    );
+    it('fails with incorrect password', () =>
+      postRequest(
+        '/v2/auth/login',
+        { login: 'rose@example.com', pwd: 'zzzzz' },
+        401
+      ));
 
-    it('fails with incorrect password', () => request(app)
-      .post('/api/v2/auth/login')
-      .send({ login: 'rose@example.com', pwd: 'zzzzz' })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(401)
-    );
-
-    it('succeeds with correct credentials', () => request(app)
-      .post('/api/v2/auth/login')
-      .send({ login: 'rose@example.com', pwd: 'abc12' })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then((res: any) => {
-        const { token } = res.body
+    it('succeeds with correct credentials', () =>
+      postRequest(
+        '/v2/auth/login',
+        { login: 'rose@example.com', pwd: 'abc12' },
+        200
+      ).then((res: any) => {
+        const { token } = res.body;
         expect(token).to.be.a('string');
-        const { login } = (jwtDecode(token) as { login: string });
+        const { login } = jwtDecode(token) as { login: string };
         expect(login).to.equal('rose@example.com');
-      })
-    );
+      }));
   });
 });
