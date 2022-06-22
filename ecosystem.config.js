01c027f38fa758038d69f9b78b07e3f6ad7d8a73
Benoît Hubert <benoithubert@gmail.com>
2022-06-22 11:28:05 +0200

    Update start script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,14 @@
 // https://stackoverflow.com/q/60581617
 
 module.exports = {
-  apps : [{
-    name: 'node-album-api',
-    // On doit passer directement le chemin vers l'"exécutable" ts-node
-    script: './node_modules/.bin/ts-node',
-    // Paramètres recommandés pour le fonctionnement en production
-    // (Diminue l'empreinte mémoire)
-    args: '--transpile-only src/index.ts',
-  }],
+  apps: [
+    {
+      name: 'node-album-api',
+      // On doit passer directement le chemin vers l'"exécutable" ts-node
+      // script: './node_modules/.bin/ts-node',
+      // Paramètres recommandés pour le fonctionnement en production
+      // (Diminue l'empreinte mémoire)
+      // args: '--transpile-only src/index.ts',
+    },
+  ],
 };
